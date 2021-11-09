7d846fb9d8cb1bca44f5d31b91924781199989e3
nabil.lahssini <nabil.lahssini@student.ehb.be>
2021-11-09 15:38:45 +0100

    CLean update repo. Base models and migration.

---
+++ EHikeB/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EHikeB.Data;
+using EHikeB.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EHikeB.Areas.Identity.IdentityHostingStartup))]
+namespace EHikeB.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EHikeBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EHikeBContextConnection")));
+
+                services.AddDefaultIdentity<Customer>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<EHikeBContext>();
+            });
+        }
+    }
+}
---
+++ EHikeB/Models/Session.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EHikeB.Models
+{
+    public enum Status
+    {
+        OPEN,
+        CLOSED,
+        TERMINATED,
+        ARCHIVED
+    }
+    public class Session
+    {
+        [Key]
+        public int SessionID { get; set; }
+        public Customer Driver { get; set; }
+        public Car Car { get; set; }
+        [Required]
+        [Display(Name = "Departure location")]
+        public string StartLocation { get; set; }
+        [Required]
+        [Display(Name = "Arrival location")]
+        public string EndLocation { get; set; }
+        [Required]
+        [Display(Name = "Departure time")]
+        public DateTime StartTime { get; set; }
+        [Required]
+        [Display(Name = "Meeting time at arrival location")]
+        public DateTime DeviationTime { get; set; }
+        [Required]
+        public Status Status { get; set; }
+        public ICollection<Customer> Hikers { get; set; }
+    }
+}
