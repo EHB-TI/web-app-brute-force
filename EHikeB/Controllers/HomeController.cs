28300e69dcf159b4c58dbc4c669bac76a84338df
OmerCanOOzdemir <omer.can.ozdemir@student.ehb.be>
2021-12-17 16:29:06 +0100

    contact us

--- EHikeB/Controllers/HomeController.cs
+++ EHikeB/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace EHikeB.Controllers
@@ -32,6 +33,35 @@ namespace EHikeB.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Contact(string name, string email, string phone, string message)
+        {
+            sendEmail(name, email, phone, message);
+
+            return Redirect("/");
+        }
+        public void sendEmail(string name, string email, string phone, string message)
+        {
+
+            MailMessage mail = new MailMessage();
+            //mail must be changed
+            mail.To.Add("EhikeEhb@outlook.com");
+            mail.From = new MailAddress("EhikeEhb@outlook.com");
+
+            mail.ReplyToList.Add("EhikeEhb@outlook.com");
+            mail.Subject = "Contact us";
+            mail.Body = "Name: " + name + "\n Email: " + email + "\n Phone: " + phone + "\n Message: " + message;
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.office365.com";
+
+            smtp.Port = 587;
+            
+            smtp.Credentials = new System.Net.NetworkCredential("EhikeEhb@outlook.com", "SoftwareSec-2020");
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
         }
     }
 }
